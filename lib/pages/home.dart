f97b3c16929d47cf0de9dd0264c556e4bad71ddd
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-05-16 12:15:14 +0300

    feat: revamped pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,12 +15,48 @@ class _HomePageState extends State<HomePage> {
     return SizedBox(
       width: MediaQuery.of(context).size.width * 0.8,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.15,
+            // width: MediaQuery.of(context).size.width * 0.8,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: CustomTextField(
+                    textController: TextEditingController(),
+                    textLabel: "Search",
+                    suffixIcon: Icons.search,
+                  ),
+                ),
+                const Spacer(),
+                CircleAvatar(
+                  backgroundColor: Color(0xFF0f305e),
+                  foregroundColor: Color(0xFF0f305e),
+                  radius: 35,
+                  child: Icon(
+                    Icons.person,
+                    size: 60,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CustomText(
+              label: "Shortcuts",
+              isBold: true,
+            ),
+          ),
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.35,
+            height: MediaQuery.of(context).size.width * 0.4,
             child: Padding(
               padding: const EdgeInsets.all(12),
-              child: Row(
+              child: Column(
                 children: [
                   DashBoardCard(
                     featureIcon: "assets/images/image_proce.png",
@@ -58,18 +94,22 @@ class DashBoardCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.2,
+      height: MediaQuery.of(context).size.height * 0.2,
+      width: MediaQuery.of(context).size.width * 0.15,
       child: Card(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CustomText(
-              label: featureName,
-              isBold: true,
-              size: 21,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CustomText(
+                label: featureName,
+                isBold: true,
+                size: 21,
+              ),
             ),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.2,
+              height: MediaQuery.of(context).size.height * 0.1,
               child: Image.asset(
                 featureIcon,
                 scale: 1,
